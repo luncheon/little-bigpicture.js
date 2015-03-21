9530f0ecf6b22f056dd7ff21e55274f7da31ca02
luncheon <takeuck@gmail.com>
2015-03-21 12:48:09 +0900

    Refactor.

--- little-bigpicture.js
+++ little-bigpicture.js
@@ -1,108 +1,101 @@
 var little = (function () {
     'use strict';
 
-    this.BigPicture = function (element) {
-        var self = this;
-
-        this.setupText = function (element) {
-            var $text = $(element);
-            return $text
-                .css('fontSize', $text.data('size') + 'px')
-                .offset({
-                    left: $text.data('x'),
-                    top:  $text.data('y'),
-                })
-                .on('blur', function (e) { $(this).text() || $(this).remove(); });
-        }
-
+    this.bigpicture = function (element) {
         var $container = $('<div>')
             .addClass('little-bigpicture-container')
             .insertBefore(element);
 
-        var $content = $(element);
-        $content.find('.text').each(function () { self.setupText(this) });
-        $content
-            .addClass('little-bigpicture-content')
-            .offset({
-                left: -$content.data('x'),
-                top:  -$content.data('y'),
-            })
-            .css('transform', 'scale(' + $content.data('zoom') + ')')
-            .appendTo($container);
+        var $view = $.extend($(element), {
+            scale: function (scale) {
+                if (scale === void 0) {
+                    return this.data('scale');
+                } else {
+                    return this.data('scale', scale).attr('data-scale', scale).css('transform', 'scale(' + scale + ')').trigger('scale', scale);
+                }
+            },
+            focusingScale: function (rate, centerX, centerY) {
+                centerX = typeof(centerX) === 'number' ? centerX : $container.width() / 2;
+                centerY = typeof(centerY) === 'number' ? centerY : $container.height() / 2;
+                return this.scale(this.scale() * rate)
+                    .x((this.x() + centerX) * rate - centerX)
+                    .y((this.y() + centerY) * rate - centerY);
+            },
+            x: function (x) {
+                if (x === void 0) {
+                    return -this.offset().left
+                } else {
+                    return this.offset({left: -x, top: this.offset().top}).trigger('offset');
+                }
+            },
+            y: function (y) {
+                if (y === void 0) {
+                    return -this.offset().top;
+                } else {
+                    return this.offset({left: this.offset().left, top: -y}).trigger('offset');
+                }
+            },
+            biggest: {
+                previous: null,
+                rectangle: function () {
+                    var left = Infinity, top = Infinity, right = -Infinity, bottom = -Infinity;
+                    $view.find('.text').each(function () {
+                        var $text = $(this);
+                        left   = Math.min(left,   $text.data('x'));
+                        top    = Math.min(top,    $text.data('y'));
+                        right  = Math.max(right,  $text.data('x') + $text.width());
+                        bottom = Math.max(bottom, $text.data('y') + $text.height());
+                    });
+                    return { left: left, top: top, right: right, bottom: bottom, width: right - left, height: bottom - top, centerX: (left + right) / 2, centerY: (top + bottom) / 2 };
+                },
+                show: function () {
+                    var previous = { offset: $view.offset(), scale: $view.scale() };
+                    var rect = this.rectangle();
+                    var scale = Math.min($container.width() / rect.width, $container.height() / rect.height) * 0.9;
+                    $view.scale(scale)
+                         .x(rect.centerX * scale - $container.width() / 2)
+                         .y(rect.centerY * scale - $container.height() / 2)
+                    this.previous = previous;
+                    return this;
+                },
+                toggle: function () {
+                    if (this.previous) {
+                        var previous = this.previous;
+                        $view.scale(previous.scale).offset(previous.offset);
+                    } else {
+                        this.show();
+                    }
+                },
+            },
+        });
 
-        // todo: rename variables and functions to more understandable.
-        var setZoom = function (zoom, left, top) {
-            $content
-                .data('zoom', zoom)
-                .attr('data-zoom', zoom)
-                .css('transform', 'scale(' + zoom + ')')
-                .offset({ left: left, top: top });
+        var setupText = function (element) {
+            var $text = $(element);
+            return $text
+                .css('fontSize', $text.data('size') + 'px')
+                .offset({ left: $text.data('x'), top: $text.data('y') })
+                .on('blur', function (e) { $(this).text() || $(this).remove(); });
         }
 
-        this.move = function (offsetX, offsetY) {
-            beforeShowBiggest = null;
-            $content.offset({
-                left: $content.offset().left + offsetX,
-                top:  $content.offset().top  + offsetY,
-            });
-            return this;
-        }
+        $view.find('.text').each(function () { setupText(this) });
+        $view.addClass('little-bigpicture-content')
+             .scale($view.data('scale') || 1)
+             .x($view.data('x'))
+             .y($view.data('y'))
+             .on('scale offset', function (e) { $view.biggest.previous = null; })
+             .appendTo($container);
 
-        this.zoom = function (rate, centerX, centerY) {
-            beforeShowBiggest = null;
-            centerX = typeof(centerX) === 'number' ? centerX : $container.width() / 2;
-            centerY = typeof(centerY) === 'number' ? centerY : $container.height() / 2;
-            setZoom(
-                $content.data('zoom') * rate,
-                ($content.offset().left - centerX) * rate + centerX,
-                ($content.offset().top  - centerY) * rate + centerY
-            );
-            return this;
-        }
-
-        this.create = function (pageX, pageY) {
-            var offset = $content.offset();
-            var zoom = $content.data('zoom');
+        var create = function (pageX, pageY) {
             var $text = $('<div>')
                 .addClass('text')
                 .attr({
                     'contentEditable': true,
-                    'data-size': 20 / zoom,
-                    'data-x': (pageX - offset.left) / zoom,
-                    'data-y': (pageY - offset.top)  / zoom,
+                    'data-size': 20 / $view.scale(),
+                    'data-x': (pageX + $view.x()) / $view.scale(),
+                    'data-y': (pageY + $view.y()) / $view.scale(),
                 });
-            self.setupText($text);
-            return $text.appendTo($content).focus();
-        }
-
-        var beforeShowBiggest = null;
-        this.showBiggest = function () {
-            var left = Infinity, top = Infinity, right = -Infinity, bottom = -Infinity;
-            $content.find('.text').each(function () {
-                var $text = $(this);
-                left   = Math.min(left,   $text.data('x'));
-                top    = Math.min(top,    $text.data('y'));
-                right  = Math.max(right,  $text.data('x') + $text.width());
-                bottom = Math.max(bottom, $text.data('y') + $text.height());
-            });
-            beforeShowBiggest = { offset: $content.offset(), zoom: $content.data('zoom') };
-            var zoom = Math.min($container.width() / (right - left), $container.height() / (bottom - top)) * 0.9;
-            setZoom(
-                zoom,
-                -((left + right) * zoom - $container.width())  / 2,
-                -((top + bottom) * zoom - $container.height()) / 2
-            );
-            return this;
-        }
-
-        this.toggleBiggest = function () {
-            if (beforeShowBiggest) {
-                setZoom(beforeShowBiggest.zoom, beforeShowBiggest.offset.left, beforeShowBiggest.offset.top);
-                beforeShowBiggest = null;
-            } else {
-                this.showBiggest();
-            }
-            return this;
+            setupText($text);
+            return $text.appendTo($view).focus();
         }
 
         var mousedown = false;
@@ -116,7 +109,8 @@ var little = (function () {
             .on('mousemove', function (e) {
                 if (mousedown && ((e.pageX - previousMouse.pageX) || (e.pageY - previousMouse.pageY))) {
                     e.preventDefault();
-                    self.move(e.pageX - previousMouse.pageX, e.pageY - previousMouse.pageY);
+                    $view.x($view.x() + previousMouse.pageX - e.pageX)
+                         .y($view.y() + previousMouse.pageY - e.pageY);
                     previousMouse = e;
                 }
             })
@@ -129,11 +123,11 @@ var little = (function () {
                 if ($(e.target).hasClass('text')) {
                     return;
                 }
-                self.create(e.pageX, e.pageY);
+                create(e.pageX, e.pageY);
             })
             .on('wheel', function (e) {
                 e.preventDefault();
-                self.zoom(
+                $view.focusingScale(
                     e.originalEvent.deltaY < 0 ? 1.6 : 0.625,
                     previousMouse && previousMouse.pageX,
                     previousMouse && previousMouse.pageY
@@ -149,10 +143,11 @@ var little = (function () {
                 var touches = e.originalEvent.touches;
                 if (touches && previousTouches) {
                     if (touches.length >= 1 && previousTouches.length >= 1) {
-                        self.move(touches[0].pageX - previousTouches[0].pageX, touches[0].pageY - previousTouches[0].pageY);
+                        $view.x($view.x() + previousTouches[0].pageX - touches[0].pageX)
+                             .y($view.y() + previousTouches[0].pageY - touches[0].pageY);
                     }
                     if (touches.length >= 2 && previousTouches.length >= 2) {
-                        self.zoom(
+                        $view.focusingScale(
                             Math.sqrt(Math.pow(touches[0].pageX - touches[1].pageX, 2) + Math.pow(touches[0].pageY - touches[1].pageY, 2)) /
                             Math.sqrt(Math.pow(previousTouches[0].pageX - previousTouches[1].pageX, 2) + Math.pow(previousTouches[0].pageY - previousTouches[1].pageY, 2)),
                             (touches[0].pageX + touches[1].pageX) / 2,
@@ -171,11 +166,12 @@ var little = (function () {
                 previousTouches = null;
                 var touches = e.originalEvent.changedTouches;
                 if (!touchmoved && touches.length === 1) {
-                    // click (unmoved touch)
-                    self.create(touches[0].pageX, touches[0].pageY);
+                    // touch and unmoved: click
+                    create(touches[0].pageX, touches[0].pageY);
                 }
             })
             ;
+        return $view;
     }
     return this;
 }).apply(this.little || {});
